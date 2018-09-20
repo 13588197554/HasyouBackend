70de537bf670a12d6314367755730ec697f1833b
david.chen <david.chen@gtedx.com>
2018-09-20 20:37:03 +0800

    update!

--- src/main/java/cn/hasyou/hasyou/HasyouApplication.java
+++ src/main/java/cn/hasyou/hasyou/HasyouApplication.java
@@ -2,10 +2,10 @@ package cn.hasyou.hasyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * 有你启动类
@@ -15,9 +15,14 @@ import java.util.Map;
 public class HasyouApplication {
 
 	@GetMapping("/hi")
-	public String hi() {
-		return "hello";
+	public ResponseEntity hi() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("hi david");
 	}
+
+	@GetMapping("/hello")
+    public ResponseEntity hello () {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("hello david");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HasyouApplication.class, args);
